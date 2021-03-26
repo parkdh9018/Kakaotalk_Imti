f6c4fbdc26a38c1416e22f9f385cb2bd60cba5a2
parkdh9018 <parkdh9018@naver.com>
2021-03-26 20:30:12 +0900

    SendMessage 진행중

---
+++ kakaoImti/kakaoImti/Connect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Connect {
+
+    [DllImport("user32.dll")]
+    public static extern int FindWindow(string lpClassName, string lpWindowName);
+    // 탑레벨 윈도우 중에서 원하는 윈도우 찾는 함수
+
+    [DllImport("user32.dll")]
+    public static extern int FindWindowEx(int hWnd1, int hWnd2, string lpClassName, string lpWindowName);
+    //자식 윈도우 찾는 함수
+
+    [DllImport("user32.dll")]
+    public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+}
+
--- kakaoImti/kakaoImti/Program.cs
+++ kakaoImti/kakaoImti/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace kakaoImti
 {
@@ -11,9 +13,33 @@ namespace kakaoImti
         /// <summary>
         /// 해당 응용 프로그램의 주 진입점입니다.
         /// </summary>
+        /// 
+
+        public const int WM_LBUTTONDOWN = 513;
+        public const int WM_KEYDOWN = 256;
+        public const int WM_KEYUP = 257;
+
+        public const int VK_TAB = 0x09;
+
         [STAThread]
         static void Main()
         {
+
+
+            int talking = Connect.FindWindow(null, "박동환");
+            Console.WriteLine(talking);
+            
+
+            if(talking > 0)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("start");
+
+                int result = Connect.SendMessage(talking, WM_KEYDOWN, Convert.ToInt32(Keys.Tab), 0);
+                Console.WriteLine(result);
+
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
